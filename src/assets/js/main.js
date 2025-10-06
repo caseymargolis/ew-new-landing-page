6961e2bea81991b684dc2c4372389c0b3e411f2a
caseymargolis <38667116+caseymargolis@users.noreply.github.com>
2025-10-07 02:00:25 +0600

    Update main.js

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -14,19 +14,44 @@ $(document).ready(function () {
     $('.navbar-collapse').collapse('hide');
   });
 
+
+  // Custom play button logic
+  document.querySelectorAll('.video-wrapper').forEach(wrapper => {
+    const video = wrapper.querySelector('video');
+    const playBtn = wrapper.querySelector('.video-play-btn');
+
+    playBtn.addEventListener('click', () => {
+      video.play();
+      wrapper.classList.add('playing');
+    });
+
+    video.addEventListener('pause', () => {
+      wrapper.classList.remove('playing');
+    });
+
+    video.addEventListener('ended', () => {
+      wrapper.classList.remove('playing');
+    });
+  });
+
   // Testimonials Video Swiper
   const testimonialVideoSwiper = new Swiper('.testimonial-video-slider', {
     slidesPerView: 1,
     spaceBetween: 30,
     loop: false,
     navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+      nextEl: '.testimonial-video-navigation .swiper-button-next',
+      prevEl: '.testimonial-video-navigation .swiper-button-prev',
     },
     // autoplay: {
     //     delay: 5000,
     //     disableOnInteraction: false,
     // },
+
+    pagination: {
+      el: '.testimonial-video-pagination',
+      clickable: true,
+    },
     breakpoints: {
       1200: {
         slidesPerView: 1,
@@ -39,32 +64,10 @@ $(document).ready(function () {
     }
   });
 
-  var testimonialSwiper = new Swiper(".testimonial-slider", {
-    slidesPerView: 1,
-    spaceBetween: 20,
-    loop: false,
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    // autoplay: {
-    //   delay: 5000,
-    //   disableOnInteraction: false,
-    // },
-    breakpoints: {
-      768: {
-        slidesPerView: 1,
-      }
-    }
-  });
-
   var featuresOneSwiper = new Swiper(".features-one-slider", {
-    slidesPerView: 1,
+    slidesPerView: 1.2, // show one full + part of the next
     spaceBetween: 20,
+    centeredSlides: false,
     loop: false,
     navigation: {
       nextEl: '.features-one-navigation .swiper-button-next',
@@ -74,29 +77,30 @@ $(document).ready(function () {
       el: ".swiper-pagination",
       clickable: true,
     },
-    // autoplay: {
-    //   delay: 5000,
-    //   disableOnInteraction: false,
-    // },
     touchEventsTarget: 'container',
     simulateTouch: true,
-    touchRatio: 1,
-    touchAngle: 45,
     grabCursor: true,
     breakpoints: {
-      768: {
+      0: { // mobile
         slidesPerView: 1,
-      }
+        spaceBetween: 15,
+      },
+      768: { // tablet
+        slidesPerView: 1,
+        spaceBetween: 20,
+      },
+      1200: { // desktop
+        slidesPerView: 1.3,
+        spaceBetween: 30,
+      },
     },
-    1200: {
-      slidesPerView: 1,
-      spaceBetween: 20
-    }
   });
 
+
   var featuresTwoSwiper = new Swiper(".features-two-slider", {
-    slidesPerView: 1,
+    slidesPerView: 1.2, // show two full + part of the next
     spaceBetween: 20,
+    centeredSlides: false,
     loop: false,
     navigation: {
       nextEl: '.features-two-navigation .swiper-button-next',
@@ -106,25 +110,62 @@ $(document).ready(function () {
       el: ".swiper-pagination",
       clickable: true,
     },
+    touchEventsTarget: 'container',
+    simulateTouch: true,
+    grabCursor: true,
+    breakpoints: {
+      0: { // mobile
+        slidesPerView: 1,
+        spaceBetween: 15,
+      },
+      768: { // tablet
+        slidesPerView: 1,
+        spaceBetween: 20,
+      },
+      1200: { // desktop
+        slidesPerView: 1.3,
+        spaceBetween: 30,
+      },
+    },
+  });
+
+  var proofMobileSwiper = new Swiper(".proofSwiper", {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    loop: false,
+    navigation: {
+      nextEl: '.proof-mobile-navigation .swiper-button-next',
+      prevEl: '.proof-mobile-navigation .swiper-button-prev',
+    },
+    pagination: {
+      el: ".proof-mobile-pagination",
+      clickable: true,
+    },
+  });
+
+  var testimonialSwiper = new Swiper(".testimonial-slider", {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    loop: false,
+    navigation: {
+      nextEl: '.testimonial-carousel-navigation .swiper-button-next',
+      prevEl: '.testimonial-carousel-navigation .swiper-button-prev',
+    },
+    pagination: {
+      el: ".testimonial-slider-pagination",
+      clickable: true,
+    },
     // autoplay: {
     //   delay: 5000,
     //   disableOnInteraction: false,
     // },
-    touchEventsTarget: 'container',
-    simulateTouch: true,
-    touchRatio: 1,
-    touchAngle: 45,
-    grabCursor: true,
     breakpoints: {
       768: {
         slidesPerView: 1,
       }
-    },
-    1200: {
-      slidesPerView: 1,
-      spaceBetween: 20
     }
   });
+
 
 });
 
